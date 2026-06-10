33c272dc4cbf063ed4bfb594c6b12427b3cdcf2f
wwyyy3 <2931070331@qq.com>
2026-06-10 08:09:41 +0800

    添加浏览器自动打开功能

--- src/main/java/com/tarot/TarotApplication.java
+++ src/main/java/com/tarot/TarotApplication.java
@@ -1,11 +1,33 @@
 package com.tarot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TarotApplication {
     public static void main(String[] args) {
         SpringApplication.run(TarotApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner openBrowser() {
+        return args -> {
+            String port = System.getProperty("server.port", "8080");
+            try {
+                Thread.sleep(1500);
+                String os = System.getProperty("os.name").toLowerCase();
+                if (os.contains("win")) {
+                    new ProcessBuilder("cmd", "/c", "start", "http://localhost:" + port).start();
+                } else if (os.contains("mac")) {
+                    new ProcessBuilder("open", "http://localhost:" + port).start();
+                } else {
+                    new ProcessBuilder("xdg-open", "http://localhost:" + port).start();
+                }
+            } catch (Exception e) {
+                // 浏览器未能自动打开，用户可以手动打开
+            }
+        };
     }
 }
